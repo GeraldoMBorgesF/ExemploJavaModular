424735ff642f431bf0ef077b0d11830c8dd08e97
Hiago Borges <geraldomborgesf@gmail.com>
2021-08-02 20:13:00 -0300

    ExemploJavaModular

---
+++ digital.innovation.one.core/src/digital/innovation/one/core/Runner.java
@@ -0,0 +1,19 @@
+package digital.innovation.one.core;
+
+import digital.innovation.one.utils.operacao.Calculadora;
+
+import java.sql.Date;
+import
+
+
+public class Runner {
+    public static void main(String[] args) {
+        Calculadora calculadora= new Calculadora();
+        new Date(2);
+
+
+        System.out.println(calculadora.sum(6,5)  );
+
+    }
+
+}
---
+++ digital.innovation.one.core/src/module-info.java
@@ -0,0 +1,6 @@
+module digital.innovation.one.core {
+
+    requires digital.innovation.one.utils;
+    requires java.sql;
+
+}
